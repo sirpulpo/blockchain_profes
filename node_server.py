632d44f6dba56388a11e2d2bd83049fdeb37e587
sirpulpo <taz.vd@hotmail.com>
2019-02-16 21:59:59 -0600

    Añadir bloques al chain.

--- node_server.py
+++ node_server.py
@@ -28,21 +28,39 @@ class Blockchain:
 
     # bloque génesis y anexarlo a la cadena. Tiene índice 0, hash anterior 0 y un hash válido.
     def create_genesis_block(self):
-        genesis_block = Block(0, [], time.time(), "0")
-        genesis_block.hash = genesis_block.compute_hash()
-        self.chain.append(genesis_block)
+        genesis_block = Block(0, [], time.time(), "0") # se instancia un objeto Block para el génesis.
+        genesis_block.hash = genesis_block.compute_hash() # se calcula su hash.
+        self.chain.append(genesis_block) # se añade el génesis al chain.
 
     @property
     def last_block(self):
-        return self.chain[-1]
+        return self.chain[-1] # verificar que pasa con el génesis, ¿se regresa a sí mismo?
 
     # se intentarán distintos valores en nonce hasta obtener un hash que satisfaga el criterio de de dificultad.
     def proof_of_work(self, block):
         block.nonce = 0
-
         computed_hash = block.compute_hash()
         while not computed_hash.startswith('0' * Blockchain. difficulty):
             block.nonce += 1
             computed_hash = block.compute_hash()
 
         return computed_hash
+
+    # verificar el block para agregarlo al chain.
+    def add_block(self, block, proof):
+        previous_hash = self.last_block.hash
+        # se verifica si el hash anterior hace referencia al referido en este blok.
+        if previous_hash != block.previus_hash:
+            return False
+        # se verifica si la prueba de trabajo es valida.
+        if not self.is_valid_proof(block, proof):
+            return False
+
+        bolck.hash = proof
+        self.chain.append(block)
+        return True
+
+    @classmethod
+    def is_valid_proof(cls, block, block_hash):
+        return (block_hash.startswith('0' * Blockchain.difficulty) and
+                block_hash == block.compute_hash())
