039ddfd65e0446589c1c1b9a45182b38c0eb63e7
sirpulpo <taz.vd@hotmail.com>
2019-02-16 23:49:42 -0600

    Se establecio función de concenso y descentralización.

--- node_server.py
+++ node_server.py
@@ -110,6 +110,9 @@ app = Flask(__name__)
 # una copia del nodo del Blockchain
 blockchain = Blockchain()
 
+# la dirección de otros miembros participantes en la red.
+peers = set()
+
 # punto de acceso para enviar nuevas transacciones (publicaciones en el blockchain).
 @app.route('/new_transaction', methods = ['POST'])
 def new_transaction():
@@ -162,4 +165,49 @@ def register_new_peers():
         peers.add(node)
 
     return 'Success', 201
-# 
+
+# punto de acceso para añadir un bloque minado.
+@app.route('/add_block', methods = ['POST'])
+def validate_and_add_block():
+    block_data = request.get_json()
+    block = Block(block_data['index'],
+                    block_data['transactions'],
+                    block_data['timestamp'],
+                    block_data['previus_hash'])
+
+    proof = block_data['hash']
+    added = blockchain.add_block(block, proof)
+
+    if not added:
+        return 'The block was discarted by the node', 400
+
+    return 'Block added to the chain', 201
+
+# algoritmo para resolver conflictos y concensuar, busca la cadena valida más larga y reemplaza la nuestra por aquella.
+def consensus():
+    global blockchain
+    longest_chain = None
+    current_len = len(blockchain.chain)
+
+    for node in peers:
+        response = request.get('http://{}/chain'.format(node))
+        length = response.json['length']
+        chain = response.json['chain']
+
+        if length > current_len and blockchain.check_chain_validity(chain):
+            current_len = length
+            longest_chain = chain
+
+    if longest_chain:
+        blockchain = longest_chain
+        return True
+
+    return False
+
+# se anuncia a la red que un nuevo bloque a sido minado.
+def announce_new_block(block):
+    for peer in peers:
+        url = 'http://{}/add_block'.format(peer)
+        request.post(url, data = json.dumps(block.__dict__, sort_keys=True))
+
+app.run(debug = True, port = 8000)
